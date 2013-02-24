59c469e1c71e39f7d01a63c02198214fd2d6c2a4
Alfonso <alfonso@libreacademia.org>
2013-02-24 20:39:01 +0100

    search module implementation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require ('express'),
+    search = require('./lib/search'),
     app = express();
-//    search = require('./lib/search');
-//    app.use('search');
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(search);
+
 app.get('/', function(req, res) {
     res.render('home');
 });
+
 app.listen(3000);
 
-
+console.log('listening on port 3000');
