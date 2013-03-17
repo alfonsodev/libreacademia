c35f8f80ab73df85231aee4361b0bf11a37948ac
Alfonso <alfonso@libreacademia.org>
2013-03-17 20:07:00 +0100

    #search, more progress

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 "use strict";
 var express = require ('express'),
-    search = require('./lib/search'),
+    Search = require('./lib/search'),
+    search = new Search(),
     app = express();
 
+app.use(express.bodyParser());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(search);
 
+//router
 app.get('/', function(req, res) {
-    search('foo', function(err, data){
+    res.render('home');
+});
+app.post('/search', function(req, res) {
+    search.query('foo', function(err, data){
         console.log(data[0]);
         res.render('home');
     });
 });
+
 
 app.listen(3000);
 
--- lib/search/README.md
+++ lib/search/README.md
@@ -1,13 +1,18 @@
 search
 ===
-This is the current search module
+Open educational resourser searcher
+it is a wrapper for Youtube, Wikipedia, Wikibooks
 
 - Usage
 
     // let's say we want results about relativity
     var search = require('search');
-    
+
     search('relativity', function(err, data){
         console.log(data);
     });
+
+- Filtering
+
+    //You want only books
 
