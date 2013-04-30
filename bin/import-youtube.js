d3f7777ff7a4b42ef243db4925030674990586bd
Alfonso <alfonso@libreacademia.org>
2013-04-30 21:57:19 +0200

    Latests changes to master

--- bin/import-youtube.js
+++ bin/import-youtube.js
@@ -1,19 +1,18 @@
-/*
-    education categoryId = 27
-*/
-var googleapis = require('googleapis');
-var doSearch = function(err, client) {
-    var params = { part: 'snippet', categoryId: "27" },
-        request = client.youtube.search.list(params);
-    client.withApiKey(process.env.GOOGLE_API_KEY);
+var googleapis = require('googleapis'),
+    GOOGLE_API_KEY = 'your api key here';
 
-    request.execute(function (err, response) {
-        if(err){
+googleapis
+  .discover('youtube', 'v3')
+  .execute(function(err, client) {
+    var params = { part: 'snippet', categoryId: '27' };
+    client.youtube.search
+      .list(params)
+      .withApiKey(GOOGLE_API_KEY)
+      .execute(function(err, response) {
+        if (err) {
             console.log(err);
         } else {
             console.log(response.items[0]);
         }
     });
-};
-
-googleapis.load('youtube', 'v3', doSearch);
+  });
